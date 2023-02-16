645e9755f09d0518cf1e0f0c8fbacb8c26b74c5f
limengda <2427330092@qq.com>
2023-02-16 14:11:24 +0800

    less配置 阿里图标引用

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,64 +1,96 @@
+
+
+<template>
+  <div class="scoll">
+
+    <header>
+      <h2 class="logo">Logo</h2>
+      <nav class="navigation">
+        <a href="#" class="active">Home</a>
+        <a href="#">About</a>
+        <a href="#">Service</a>
+        <a href="#">Contact</a>
+      </nav>
+    </header>
+
+    <section class="parallax">
+      <img src="/home/hill1.png" id="hill1" ref="hill1" />
+      <img src="/home/hill2.png" id="hill2" />
+      <img src="/home/hill3.png" id="hill3" />
+      <img src="/home/hill4.png" id="hill4" ref="hill4" />
+      <img src="/home/hill5.png" id="hill5" ref="hill5" />
+      <img src="/home/tree.png" id="tree" />
+      <h2 ref="text" id="text">Parallax Website</h2>
+      <img src="/home/leaf.png" ref="leaf" id="leaf" />
+      <img src="/home/plant.png" id="plant" />
+      <div class="downScoll" v-if="downScoll">
+        <span class="iconfont icon-xiangxia"></span>
+        <span class="iconfont icon-xiangxia"></span>
+        <span class="iconfont icon-xiangxia"></span>
+      </div>
+    </section>
+
+    <section class="sec">
+      <h2>Parallax Scrolling Website</h2>
+      <Menu />
+    </section>
+</div>
+</template>
 <script setup lang="ts">
+
+
+
+
+
+
+
+
+
+
+
+
 import { ref, onMounted } from "vue";
+
 import Menu from "./menu.vue";
-const text = ref(null);
-const leaf = ref(null);
-const hill1 = ref(null);
-const hill4 = ref(null);
-const hill5 = ref(null);
+const downScoll = ref<boolean>(true)
+const text = ref<HTMLDivElement | null>(null);
+const leaf = ref<HTMLDivElement | null>(null);
+const hill1 = ref<HTMLDivElement | null>(null);
+const hill4 = ref<HTMLDivElement | null>(null);
+const hill5 = ref<HTMLDivElement | null>(null);
 onMounted(() => {
   window.addEventListener("scroll", () => {
     let value = window.scrollY;
-    text.value.style.marginTop = value * 1.5 + "px";
-    leaf.value.style.top = value * -1.5 + "px";
-    leaf.value.style.left = value * 1.5 + "px";
-    hill5.value.style.left = value * 1.5 + "px";
-    hill4.value.style.left = value * -1.5 + "px";
-    hill1.value.style.top = value * 0.5 + "px";
+    downScoll.value = (value == 0)
+    text.value && (text.value.style.marginTop = value * 1.5 + "px");
+    leaf.value && (leaf.value.style.top = value * -1.5 + "px");
+    leaf.value && (leaf.value.style.left = value * 1.5 + "px");
+    hill5.value && (hill5.value.style.left = value * 1.5 + "px");
+    hill4.value && (hill4.value.style.left = value * -1.5 + "px");
+    hill1.value && (hill1.value.style.top = value * 0.5 + "px");
   });
 });
 </script>
 
-<template>
-  <div class="scoll">
-  <header>
-    <h2 class="logo">Logo</h2>
-    <nav class="navigation">
-      <a href="#" class="active">Home</a>
-      <a href="#">About</a>
-      <a href="#">Service</a>
-      <a href="#">Contact</a>
-    </nav>
-  </header>
-
-  <section class="parallax">
-    <img src="/home/hill1.png" id="hill1" ref="hill1" />
-    <img src="/home/hill2.png" id="hill2" />
-    <img src="/home/hill3.png" id="hill3" />
-    <img src="/home/hill4.png" id="hill4" ref="hill4" />
-    <img src="/home/hill5.png" id="hill5" ref="hill5" />
-    <img src="/home/tree.png" id="tree" />
-    <h2 ref="text" id="text">Parallax Website</h2>
-    <img src="/home/leaf.png" ref="leaf" id="leaf" />
-    <img src="/home/plant.png" id="plant" />
-  </section>
-
-  <section class="sec">
-    <h2>Parallax Scrolling Website</h2>
-   <Menu />
-  </section>
-</div>
-</template>
-
-<style scoped>
-@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap");
-
+<style scoped lang="less">
 * {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
   font-family: "Poppins", sans-serif;
 }
+
+.downScoll {
+  z-index: 120;
+  display: flex;
+  flex-direction: column;
+
+  .icon-xiangxia {
+    color: #fff;
+
+  }
+}
+
 
 ::selection {
   background: rgba(0, 0, 0, 0.2);
@@ -69,9 +101,11 @@ body {
   min-height: 100vh;
   overflow-x: hidden;
 }
-.scoll{
+
+.scoll {
   overflow: hidden;
 }
+
 header {
   position: absolute;
   top: 0;
@@ -112,6 +146,14 @@ header {
   justify-content: center;
   align-items: center;
   height: 100vh;
+
+  img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    pointer-events: none;
+  }
 }
 
 #text {
@@ -121,30 +163,23 @@ header {
   text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
 }
 
-.parallax img {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  pointer-events: none;
-}
 
 .sec {
   position: relative;
   background: #003329;
   padding: 100px;
   overflow: hidden;
-}
 
-.sec h2 {
-  font-size: 3em;
-  color: #fff;
-  margin-bottom: 10px;
-}
+  h2 {
+    font-size: 3em;
+    color: #fff;
+    margin-bottom: 10px;
+  }
 
-.sec p {
-  font-size: 1em;
-  color: #fff;
-  font-weight: 300;
+  p {
+    font-size: 1em;
+    color: #fff;
+    font-weight: 300;
+  }
 }
 </style>
