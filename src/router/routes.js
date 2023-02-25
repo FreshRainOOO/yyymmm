5d58b269d4ffee4a6860bea7414b14adb974f2e7
limengda <2427330092@qq.com>
2023-02-25 15:15:29 +0800

    修改/跟路由

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,15 +1,15 @@
 const routes = [
-    {
-        path: '/',
-        name: 'index',
-        title: '首页',
-        component: () => import('../views/home/index.vue'), 
-    },
-    {
-        path: '/test',
-        name: 'test',
-        title: '测试',
-        component: () => import('../views/test/index.vue'), 
-    }
-]
-export default routes
+  {
+    path: "/da-blog/",
+    name: "index",
+    title: "首页",
+    component: () => import("../views/home/index.vue"),
+  },
+  {
+    path: "/test",
+    name: "test",
+    title: "测试",
+    component: () => import("../views/test/index.vue"),
+  },
+];
+export default routes;
