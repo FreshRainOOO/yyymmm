7ba94a6d5bbc04645ee6c5e0a2c094c8cf91c2e7
limengda <2427330092@qq.com>
2023-02-25 11:59:52 +0800

    部署

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import vue from '@vitejs/plugin-vue'
 import { plugin } from 'vite-plugin-markdown'
 import * as path from 'path'
 export default defineConfig({
-  plugins: [vue(),  plugin({
+  plugins: [vue(), plugin({
     mode: ["html"],
   })],
+  base: '/da-blog/',//GitHub部署目录名
   css: {
     // 预处理器配置项
     preprocessorOptions: {
