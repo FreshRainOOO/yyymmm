ea7d6daf12aa4b899c2ce713b43a9063d79d1bcd
limengda <2427330092@qq.com>
2023-02-27 18:16:41 +0800

    src

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,13 +3,13 @@ const routes = [
     path: "/da-blog/",
     name: "index",
     title: "首页",
-    component: () => import("../views/home/index.vue"),
+    component: () => import("@/views/home/index.vue"),
   },
   {
     path: "/da-blog/test",
     name: "test",
     title: "测试",
-    component: () => import("../views/test/index.vue"),
+    component: () => import("@/views/test/index.vue"),
   },
 ];
 export default routes;
